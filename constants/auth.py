fb5fd36de25a204d545a68ecb3e0516f4b67d6c6
Baik2222 <baik2221982@yandex.ru>
2025-10-05 07:23:18 +0100

    Constants

--- constants/auth.py
+++ constants/auth.py
@@ -2,3 +2,6 @@ from .utils import generate_email, generate_password
 
 TEST_EMAIL = generate_email()
 TEST_PASSWORD = generate_password(10)
+
+AUTH_TEST_EMAIL = "mymail@yandex.ru"
+AUTH_TEST_PASSWORD = "Strong123"
